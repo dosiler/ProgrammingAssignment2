9f87083b5deb1888254a47b972c2bed7194a36e8
DonS <dosiler@users.noreply.github.com>
2016-05-29 20:56:23 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,18 +1,23 @@
-makeCacheMatrix <- function(x = matrix()) {   ## Create a function called "makeCacheMatrix", which takes one argument and coerces it to be a matrix
+makeCacheMatrix <- function(x = matrix()) {   ## Create a function called "makeCacheMatrix", which takes 
+                                              ## one argument and coerces it to be a matrix
   m <- NULL                             ## Initialize the variable "m" as a NULL
   set <- function(y) {                  ## Create a sub-function called "set", which takes an argument "y"
-    x <<- y                       ## Part of the "set" sub-function; sets "x" to "y" within the function "makeCacheMatrix", not just the sub-function
+    x <<- y                       ## Part of the "set" sub-function; sets "x" to "y" within the function 
+                                  ## "makeCacheMatrix", not just the sub-function
     m <<- NULL                    ## Also part of "set"; initializes "m". We're telling the function that 
-    ## we've made a new matrix, likely with a different "m" (inverted matrix)
+                                  ## we've made a new matrix, likely with a different "m" (inverted matrix)
   }
-  get <- function() x                   ## Create a sub-function called "get" reporting the value of x. The "x" comes after the "function()" 
-  ## because this way, the function takes no arguments, it just prints x
-  setim <- function(solve) m <<- solve  ## Create a sub-function called "setim", which works mostly through its interaction with the 
-  ## cacheSolve function, by applying solve() to the vector data before using setim to assign a 
-  ## value to m. It then assigns the result of the solve function to "m". 
+  get <- function() x                   ## Create a sub-function called "get" reporting the value of x. The 
+                                        ## "x" comes after the "function()" because this way, the function 
+					## takes no arguments, it just prints x
+  setim <- function(solve) m <<- solve  ## Create a sub-function called "setim", which works mostly through 
+                                        ## its interaction with the cacheSolve function, by applying solve() 
+					## to the vector data before using setim to assign a value to m. It 
+					## then assigns the result of the solve function to "m". 
   getim <- function() m                 ## Create a sub-function called "getim", which reports the value of "m"
-  list(set = set, get = get,            ## Create the list itself. For each element of the list, the part before the "=" indicates the name or 
-       setim = setim,                   ## key to be used to call that item, and the part after identifies the item (in this case, the sub-function) 
+  list(set = set, get = get,            ## Create the list itself. For each element of the list, the part 
+                                        ## before the "=" indicates the name or key to be used to call that item, 
+       setim = setim,                   ## and the part after identifies the item (in this case, the sub-function) 
        getim = getim)                   ## that the name or key will call.
 }
 
